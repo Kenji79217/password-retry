bbecd7ee7e99b2d8448a8a1cb04c1f3a37a7ce27
Kenji79217 <47220727+Kenji79217@users.noreply.github.com>
2019-02-07 22:21:27 +0800

    Update password-retry.py

--- password-retry.py
+++ password-retry.py
@@ -1,12 +1,14 @@
 password = 'a123456'
-i = 3
-while True:
+i = 3  #剩餘機會
+while i > 0:
+	i = i - 1
 	pwd = input('請輸入密碼： ')
 	if pwd == password:
 		print('登入成功！')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤! 還有', i, '次機會')
-		if i == 0:
-			break
+		print('密碼錯誤! ')
+		if i > 0:
+			print('還有', i, '次機會')
+		else:
+			print('沒機會嘗試了！在想一下吧～ ')
